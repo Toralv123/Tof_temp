33d9fb52e3a22b4e9dd2a8943d1ba70fa2fd836a
Toralv123 <160240498+Toralv123@users.noreply.github.com>
2024-03-15 14:01:33 +0100

    Oppdater pxt.json, main.blocks, main.ts, 无标题.json

--- main.ts
+++ main.ts
@@ -1,16 +1,47 @@
-let UV = 0
-let Temp = 0
+let RH_i_bakken = 0
 let RH = 0
+let Temp = 0
 OLED.init(128, 64)
+datalogger.setColumnTitles(
+"Temp:",
+"RH i Bakken:",
+"RH:"
+)
+ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+ESP8266_IoT.connectWifi("Helios", "Toralv2110")
+ESP8266_IoT.connectThingSpeak()
+ESP8266_IoT.setData(
+"25YC8X8T5ITHGG2Y",
+Temp,
+RH,
+RH_i_bakken
+)
 basic.forever(function () {
-    RH = smarthome.dht11Sensor(DigitalPin.P1, smarthome.DHT11_state.DHT11_humidity)
-    basic.pause(1000)
-    Temp = smarthome.dht11Sensor(DigitalPin.P1, smarthome.DHT11_state.DHT11_temperature_C)
-    basic.pause(1000)
-    UV = smarthome.UVLevel(AnalogPin.P3)
-    basic.pause(1000)
+    RH = Environment.dht11value(Environment.DHT11Type.DHT11_humidity, DigitalPin.P1)
+    Temp = Environment.dht11value(Environment.DHT11Type.DHT11_temperature_C, DigitalPin.P1)
+    RH_i_bakken = Environment.ReadSoilHumidity(AnalogPin.P2)
+    datalogger.log(
+    datalogger.createCV("RH:", RH),
+    datalogger.createCV("Temp:", Temp),
+    datalogger.createCV("RH i Bakken:", RH_i_bakken)
+    )
     OLED.clear()
+    if (ESP8266_IoT.wifiState(true)) {
+        OLED.writeStringNewLine("Wife koblet til")
+    } else {
+        OLED.writeStringNewLine("Wife ikke koblet til")
+    }
+    if (ESP8266_IoT.thingSpeakState(true)) {
+        OLED.writeStringNewLine("ThingsSpeak koblet til")
+    } else {
+        OLED.writeStringNewLine("ThingsSpeak ikke koblet til")
+    }
     OLED.writeStringNewLine("Temp:" + Temp)
     OLED.writeStringNewLine("RH:" + RH)
-    OLED.writeStringNewLine("UV:" + UV)
+    OLED.writeStringNewLine("RH i bakken:" + RH_i_bakken)
+    ESP8266_IoT.uploadData()
+    basic.pause(7000)
+})
+loops.everyInterval(86400000, function () {
+	
 })
